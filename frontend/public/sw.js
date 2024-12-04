31c5898d5a21f788aaef2c76ea241df5e4eb6060
Mitr Joshi <mitrjoshi26@gmail.com>
2024-12-04 17:28:35 +0530

    1

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,13 +1,16 @@
 self.addEventListener("push", (event) => {
-  const data = event.data?.json() || {};
+  console.log("Push event received:", event);
+
+  const data = event.data?.json() || {
+    title: "Default Title",
+    body: "Default Body",
+  };
   const { title, body } = data;
 
   const options = {
     body: body || "Default notification body",
-    icon: "/sprite.svg",
+    icon: "/icon-192x192.png", // Path to your app's icon
   };
 
-  event.waitUntil(
-    self.registration.showNotification(title || "Notification", options)
-  );
+  event.waitUntil(self.registration.showNotification(title, options));
 });
